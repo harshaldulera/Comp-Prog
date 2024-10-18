43b7df35202293268345b25c56b968fbbbd17bcb
Harshal Dulera <harshaldulera82@gmail.com>
2024-10-18 10:28:29 +0530

    lc & code360

---
+++ code360/intersection-of-2-arrays.cpp
@@ -0,0 +1,41 @@
+/* 
+Brute Force Approach: TLE
+
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m) {
+	vector<int> common;
+	vector<bool> used(m, false);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+			if(arr1[i] < arr2[j]) {
+				break;
+			}
+			if(arr1[i] == arr2[j] && !used[j]) {
+				common.emplace_back(arr1[i]);
+				used[j] = true;
+				break;
+			}
+		}
+	}
+	return common;
+}
+*/
+
+
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m) {
+	int i =0; int j = 0;
+	vector<int> common;
+	while(i < n && j < m) {
+		if (arr1[i] < arr2[j]){
+			i++;
+		} else if (arr1[i] == arr2[j]) {
+			common.emplace_back(arr1[i]);
+			i++;
+			j++;
+		} else if (arr1[i] > arr2[j]) {
+			j++;
+		}
+	}
+	return common;
+}
