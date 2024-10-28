deedc60057e4a25d21e2131f6443cfff7799fcb3
Harshal Dulera <harshaldulera82@gmail.com>
2024-10-28 17:42:46 +0530

    lc: sqrtx

---
+++ Leetcode/sqrtx.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long int binarySearch(int n) {
+        int start = 0;
+        int end = n - 1;
+        long long int mid = start + (end - start) / 2;
+
+        long long int ans = -1;
+
+        while(start <= end) {
+            long long int square = mid * mid;
+            if(square == n) {
+                return mid;
+            } 
+
+            if(square < n) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return ans;
+    }
+
+    int mySqrt(int x) {
+        return binarySearch(x);
+    }
+};
