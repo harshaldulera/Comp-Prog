23c5dd6c6e81b21b80306fc801e01001179490f5
Harshal Dulera <harshaldulera82@gmail.com>
2024-10-29 13:23:34 +0530

    Refactor maximum-subarray.cpp for improved readability and performance

--- Leetcode/maximum-subarray.cpp
+++ Leetcode/maximum-subarray.cpp
@@ -1,12 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        //sort(nums.begin(), nums.end());
         long long maxi = LONG_MIN;
         long long sum = 0;
-        //if(nums.size() < 2) return 0;
-
-        // int maxSum = nums[0] + nums[1];
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
 
@@ -14,9 +13,9 @@ public:
                 maxi = sum;
             }
 
-            if(sum < 0) {
+            /*if(sum < 0) {
                 sum = 0;
-            }
+            }*/
         }
         return maxi;  
     }
