f5d34d72be103c566b7ee0ae4d6b517833c944aa
Harshal Dulera <harshaldulera82@gmail.com>
2024-11-03 12:19:14 +0530

    feat: add solution to check if an array is sorted and rotated, and enhance rotate array implementation

--- Leetcode/rotate-array.cpp
+++ Leetcode/rotate-array.cpp
@@ -18,3 +18,17 @@ public:
         reverseArray(nums, k, nums.size() - 1);
     }
 };
+
+
+// Second Approach
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+        for(int i = 0; i < nums.size(); i++) {
+            temp[(i + k) % nums.size()] = nums[i];
+        }    
+        nums = temp;
+    }
+};
